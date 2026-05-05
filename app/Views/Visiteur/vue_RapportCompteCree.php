290c7f4c6e4756f5dac13fcd8baee496958d7029
My Name <thesame@email.com>
2026-05-05 07:45:24 +0200

    Amélioration de la création de compte

--- app/Views/Visiteur/vue_RapportCompteCree.php
+++ app/Views/Visiteur/vue_RapportCompteCree.php
@@ -1,3 +1,3 @@
-<h2>Création réussie !</h2>
+<center><h2>Création réussie !</h2>
 <?php echo '<p>Bienvenue '.$Prenom.' !</p>';?>
-<p><a href="<?php echo site_url('seconnecter') ?>">Se connecter</a><p>
+<p><a href="<?php echo site_url('seconnecter') ?>" class="btn btn-light">Se connecter</a></p></center>
